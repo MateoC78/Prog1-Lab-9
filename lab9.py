d9435b05b9f0deb6f26e7de04e20c50fbd5e4706
Joseph Robinson <166070458+Robinson-Joseph@users.noreply.github.com>
2024-04-09 21:35:07 -0400

    Added decoder function.

--- lab9.py
+++ lab9.py
@@ -3,6 +3,14 @@ def encoder(password):
          password += (3*10**i)
     return password
 
+
+def decoder(password):
+    decoded = ""
+    for i in password:
+        decoded = decoded + str(int(i)-3)
+    return decoded
+
+
 if __name__ == "__main__":
     while True:
         print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
