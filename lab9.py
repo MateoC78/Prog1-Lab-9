dbba52b3a1d65c13c0c03d560b16dc24b9676bee
Mateo <matartcar@gmail.com>
2024-04-10 13:19:10 -0400

    hi

--- lab9.py
+++ lab9.py
@@ -1,9 +1,34 @@
-def encoder(password):
+def encoder(p):
+    x = [int(y) for y in str(p)]
+    y = ""
     for i in range(8):
-         password += (3*10**i)
-    return password
+        x[i] += 3
+        if x[i] > 9:
+            x[i] -= 10
+        y += str(x[i])
+    return y
+
+def decoder(p):
+    x = [int(y) for y in str(p)]
+    y = ""
+    for i in range(8):
+        x[i] -= 3
+        if x[i] < 0:
+            x[i] += 10
+        y += str(x[i])
+    return y
 
 if __name__ == "__main__":
+    print(decoder(12345897))
     while True:
-        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
+        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
         choice = input("Please enter an option: ")
+        if choice == '1':
+            password = int(input("Please enter your password to encode: "))
+            x = encoder(password)
+            print("Your password has been encoded and stored!\n")
+        elif choice == '2':
+            print(f"The encoded password is {x}, and the original password is {decoder(x)}.\n")
+            pass
+        elif choice == '3':
+            break
